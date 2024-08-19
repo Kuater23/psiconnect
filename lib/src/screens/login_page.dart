d6d5aae0c3c4408e555473c2410256d4d782bff0
Kuater23 <95001390+Kuater23@users.noreply.github.com>
2024-08-19 00:25:28 -0300

    sumo el login_page al reboot

--- lib/src/screens/login_page.dart
+++ lib/src/screens/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:Psiconnect/src/service/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // Importa Firebase Auth
 
 class LoginPage extends StatefulWidget {
   @override
@@ -7,95 +8,42 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final AuthService _authService = AuthService();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  String _errorMessage = '';
-
-  void _login() async {
-    String email = _emailController.text;
-    String password = _passwordController.text;
-
-    try {
-      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      // Navegar a la página principal o mostrar un mensaje de éxito
-    } on FirebaseAuthException catch (e) {
-      setState(() {
-        _errorMessage = e.message ?? 'Error desconocido';
-      });
-    }
-  }
-
-  void _loginWithGoogle() async {
-    GoogleAuthProvider googleProvider = GoogleAuthProvider();
-    googleProvider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-    googleProvider.setCustomParameters({
-      'login_hint': 'user@example.com'
-    });
-
-    try {
-      UserCredential userCredential = await _auth.signInWithPopup(googleProvider);
-      // Navegar a la página principal o mostrar un mensaje de éxito
-    } on FirebaseAuthException catch (e) {
-      setState(() {
-        _errorMessage = e.message ?? 'Error desconocido';
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login Page'),
+        title: Text('Login'),
       ),
-      body: Center( // Envolver en Center para centrar todo
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min, // Ajustar el tamaño de la columna
-            children: [
-              Container(
-                width: 300, // Ajusta el ancho según sea necesario
-                child: TextField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-              ),
-              Container(
-                width: 300, // Ajusta el ancho según sea necesario
-                child: TextField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                  ),
-                  obscureText: true,
-                  keyboardType: TextInputType.visiblePassword,
-                ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _login,
-                child: Text('Login'),
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: _loginWithGoogle,
-                child: Text('Login with Google'),
-              ),
-              if (_errorMessage.isNotEmpty)
-                Text(
-                  _errorMessage,
-                  style: TextStyle(color: Colors.red),
-                ),
-            ],
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                User? user = await _authService.signInWithEmailAndPassword(
+                  _emailController.text,
+                  _passwordController.text,
+                );
+                if (user != null) {
+                  Navigator.pushReplacementNamed(context, '/');
+                }
+              },
+              child: Text('Login'),
+            ),
+          ],
         ),
       ),
     );
