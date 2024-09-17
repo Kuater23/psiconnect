222a9b7a01be9df09a7bc042d934406ebd4b95a0
luca-invernizzi-davinci <89217721+luca-invernizzi-davinci@users.noreply.github.com>
2024-09-17 00:06:20 -0300

    Mejoras en la seccion de profesional para manejo de citas y pacientes - incompleto

--- lib/src/screens/professional/professional_files.dart
+++ lib/src/screens/professional/professional_files.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:Psiconnect/src/widgets/shared_drawer.dart'; // Importa el Drawer compartido
 
 class ProfessionalFiles extends StatelessWidget {
+  final String patientId; // Paciente ID que se pasará desde la cita
+
+  const ProfessionalFiles({Key? key, required this.patientId}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Archivos por Paciente'),
+        title: Text('Historial Clínico de $patientId'),
       ),
-      drawer: SharedDrawer(), // Añade el menú hamburguesa compartido
       body: Center(
-        child: Text('Aquí se mostrarán los archivos por paciente.'),
+        child: Text('Aquí se mostrarán los archivos médicos completos del paciente con ID $patientId.'),
       ),
     );
   }
